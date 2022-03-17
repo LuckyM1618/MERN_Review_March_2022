333c0ca77f9eb41555c7ac055ea3c8e8ddd44584
Lucky <lucky.maker1.618@gmail.com>
2022-03-17 14:33:07 -0500

    Completed First React Assignment

---
+++ react/test_react/test_react_project/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>We have control</p>
+        <a
+          className="App-link"
+          href="https://matrix.fandom.com/wiki/Main_Page"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn The Truth
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
